13540d2415476aceb062fe71e65fbc328e940e82
watame <pandora.border@gmail.com>
2023-10-03 22:16:35 +0900

    :+1: add project model validation test

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
+
+  # プロジェクト名がなければ登録できないこと
+  it "is invalid without a name" do
+    user = User.create(
+      first_name: "Joe",
+      last_name:  "Tester",
+      email:      "joetester@example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+    )
+
+    project = user.projects.new(
+      name: nil,
+    )
+
+    project.valid?
+    expect(project.errors[:name]).to include("can't be blank")
+  end
+
   # ユーザー単位では重複したプロジェクト名を許可しないこと
   it "does not allow duplicate project names per user" do
     user = User.create(
