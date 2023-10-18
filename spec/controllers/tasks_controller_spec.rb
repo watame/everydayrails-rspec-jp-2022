17e092d4276abeb6453c514075303f130774f065
watame <pandora.border@gmail.com>
2023-10-18 23:38:34 +0900

    add tasks controller test

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -17,4 +17,35 @@ RSpec.describe TasksController, type: :controller do
     end
   end
 
+  describe "#create" do
+    # JSON 形式でレスポンスを返すこと
+    it "responds with JSON formatted output" do
+      new_task = { name: "New test task" }
+      sign_in @user
+      post :create, format: :json,
+        params: { project_id: @project.id, task: new_task }
+      expect(response.content_type).to include "application/json"
+    end
+
+    # 新しいタスクをプロジェクトに追加すること
+    it "adds a new task to the project" do
+      new_task = { name: "New test task" }
+      sign_in @user
+      expect {
+        post :create, format: :json,
+          params: { project_id: @project.id, task: new_task }
+      }.to change(@project.tasks, :count).by(1)
+    end
+
+      # 認証を要求すること
+    it "requires authentication" do
+      new_task = { name: "New test task" }
+      # 未ログイン状態でやる
+      expect {
+        post :create, format: :json,
+          params: { project_id: @project.id, task: new_task }
+      }.to_not change(@project.tasks, :count)
+      expect(response).to_not be_successful
+    end
+  end
 end
