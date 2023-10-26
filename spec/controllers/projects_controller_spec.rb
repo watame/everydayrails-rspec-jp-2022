097aa120fc330f466d9d265d302b39f59e2478e6
watame <pandora.border@gmail.com>
2023-10-26 21:40:21 +0900

    add project controller new, edit test

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -106,6 +106,111 @@ RSpec.describe ProjectsController, type: :controller do
     end
   end
 
+  describe "#new" do
+    # 認証済みユーザーとして
+    context "as an authenticated user" do
+      before do
+        @user = FactoryBot.create(:user)
+      end
+
+      # 正常にレスポンスを返すこと
+      it "responds successfully" do
+        sign_in @user
+        get :new
+        expect(response).to be_successful
+      end
+
+      # 200レスポンスを返すこと
+      it "returns a 200 response" do
+        sign_in @user
+        get :new
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    # ゲストとして
+    context "as a guest" do
+      # サインイン画面にリダイレクトすること
+      it "redirects to the sign-in page" do
+        get :new
+        expect(response).to redirect_to "/users/sign_in"
+      end
+
+      # 302レスポンスを返すこと
+      it "returns a 302 response" do
+        get :new
+        expect(response).to have_http_status(:found)
+      end
+    end
+  end
+
+  describe "#edit" do
+    # 認証済みユーザーとして
+    context "as an authenticated user" do
+      before do
+        @user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: @user)
+      end
+
+      # 正常にレスポンスを返すこと
+      it "responds successfully" do
+        sign_in @user
+        get :edit, params: { id: @project.id }
+        expect(response).to be_successful
+      end
+
+      # 200レスポンスを返すこと
+      it "returns a 200 response" do
+        sign_in @user
+        get :edit, params: { id: @project.id }
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    # 認可されていないユーザーとして
+    context "as an unauthorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        other_user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: other_user)
+      end
+
+      # サインイン画面にリダイレクトすること
+      it "redirects to the dashboard" do
+        sign_in @user
+        get :edit, params: { id: @project.id }
+        expect(response).to redirect_to root_path
+      end
+
+      # 302レスポンスを返すこと
+      it "returns a 302 response" do
+        sign_in @user
+        get :edit, params: { id: @project.id }
+        expect(response).to have_http_status(:found)
+      end
+    end
+
+    # ゲストとして
+    context "as a guest" do
+      before do
+        other_user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: other_user)
+      end
+
+      # サインイン画面にリダイレクトすること
+      it "redirects to the sign-in page" do
+        get :edit, params: { id: @project.id }
+        expect(response).to redirect_to "/users/sign_in"
+      end
+
+      # 302レスポンスを返すこと
+      it "returns a 302 response" do
+        get :edit, params: { id: @project.id }
+        expect(response).to have_http_status(:found)
+      end
+    end
+  end
+
   describe "#create" do
     # 認証済みユーザーとして
     context "as an authenticated user" do
